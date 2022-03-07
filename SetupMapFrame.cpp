a066ceb8d38f737d2d3e9167d7acc48ad750c852
William Pederzoli <wpederzoli@gmail.com>
2022-03-07 15:30:50 +0100

    Setup controllers in place

--- SetupMapFrame.cpp
+++ SetupMapFrame.cpp
@@ -6,7 +6,15 @@ wxEND_EVENT_TABLE()
 SetupMapFrame::SetupMapFrame(wxFrame* parent) : wxFrame(parent, wxID_ANY, "Setup map", wxDefaultPosition, wxSize(400, 200))
 {
     mainPanel = new wxPanel(this, wxID_ANY);
-    input = new wxTextCtrl(mainPanel, wxID_ANY);
+    colsLabel = new wxStaticText(mainPanel, wxID_ANY, "Columns: ", wxPoint(wxDefaultPosition.x + 10, wxDefaultPosition.y + 15), wxSize(100, 50) );
+    colsInput = new wxSpinCtrl(mainPanel, wxID_ANY, wxEmptyString, wxPoint(colsLabel->GetPosition().x + 100, colsLabel->GetPosition().y - 10 ), wxSize(150, 25 ) );
+    rowsLabel = new wxStaticText(mainPanel, wxID_ANY, "Rows: ", wxPoint(colsLabel->GetPosition().x, colsLabel->GetPosition().y + 30), wxSize(colsLabel->GetSize() ) );
+    rowsInput = new wxSpinCtrl(mainPanel, wxID_ANY, wxEmptyString, wxPoint(colsLabel->GetPosition().x + 100, rowsLabel->GetPosition().y - 10), wxSize(150, 25 ) );
+    tileWidthLabel = new wxStaticText(mainPanel, wxID_ANY, "Tiles width: ", wxPoint(rowsLabel->GetPosition().x, rowsLabel->GetPosition().y + 30), wxSize(100, 25) );
+    tileWidthInput = new wxSpinCtrl(mainPanel, wxID_ANY, wxEmptyString, wxPoint(colsLabel->GetPosition().x + 100, tileWidthLabel->GetPosition().y - 10), wxSize(150, 25) );
+    tileHeightLabel = new wxStaticText(mainPanel, wxID_ANY, "Tiles height: ", wxPoint(tileWidthLabel->GetPosition().x, tileWidthLabel->GetPosition().y + 30), wxSize(100, 25) );
+    tileWidthInput = new wxSpinCtrl(mainPanel, wxID_ANY, wxEmptyString, wxPoint(colsLabel->GetPosition().x + 100, tileHeightLabel->GetPosition().y - 10), wxSize(150, 25) );
+
     mainPanel->Show(true);
 
     Show(true);
--- SetupMapFrame.h
+++ SetupMapFrame.h
@@ -2,12 +2,23 @@
 #define SETUPMAPFRAME_H
 
 #include <wx/wx.h>
+#include <wx/spinctrl.h>
 
 class SetupMapFrame : public wxFrame
 {
     private:
         wxPanel* mainPanel;
-        wxTextCtrl* input;
+        wxStaticText* colsLabel;
+        wxSpinCtrl* colsInput;
+        wxStaticText* rowsLabel;
+        wxSpinCtrl* rowsInput;
+        wxStaticText* tileWidthLabel;
+        wxSpinCtrl* tileWidthInput;
+        wxStaticText* tileHeightLabel;
+        wxSpinCtrl* tileHeigthInput;
+
+        wxTextCtrl* mapName;
+
 
     public:
 
