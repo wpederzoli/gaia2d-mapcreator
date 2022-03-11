3b24d9600634b634e14ddf23bd41959ab0eade9a
William Pederzoli <wpederzoli@gmail.com>
2022-03-11 10:35:30 +0100

    Added debugger. Images positioning. Images clickable

--- LoadAssetsPanel.cpp
+++ LoadAssetsPanel.cpp
@@ -2,30 +2,61 @@
 
 wxBEGIN_EVENT_TABLE(LoadAssetsPanel, wxPanel)
     EVT_BUTTON(12, LoadAssetsPanel::OnLoadBtnClick)
+    EVT_BUTTON(wxID_ANY, LoadAssetsPanel::OnImageClick)
 wxEND_EVENT_TABLE()
 
 LoadAssetsPanel::LoadAssetsPanel(wxWindow* parent) : wxPanel(parent, wxID_ANY)
 {
+    wxInitAllImageHandlers();
     loadAssetsBtn = new wxButton(this, 12, "Load", wxPoint(20, 20), wxDefaultSize);
 
     SetBackgroundColour(wxColor(200, 133, 233));
 };
 
-LoadAssetsPanel::~LoadAssetsPanel(){};
+LoadAssetsPanel::~LoadAssetsPanel()
+{
+    delete loadAssetsBtn;
+    while(!bm.empty() )
+    {
+        delete bm.top();
+        bm.pop();
+    }
+};
 
 void LoadAssetsPanel::OnLoadBtnClick(wxCommandEvent& evt) 
 {
     wxFileDialog openFile(this, "Select file", "", "", "PNG and JPG files  (*.png;*.jpg;*.jpeg)|*.png;*.jpg;*.jpeg", wxFD_OPEN|wxFD_FILE_MUST_EXIST);
+    
     if(openFile.ShowModal() == wxID_OK)
     {
-        wxPNGHandler* handler = new wxPNGHandler;
-        wxImage::AddHandler(handler);
         wxString path = openFile.GetPath();
-        wxImage* image = new wxImage;
-        image->LoadFile(path, wxBITMAP_TYPE_PNG);
-        image->Rescale(50, 50);
-        wxStaticBitmap* im = new wxStaticBitmap(this, wxID_ANY, wxBitmap((*image), wxBITMAP_TYPE_PNG), wxPoint(200, 10));
+        wxImage image;
+        image.LoadFile(path, wxBITMAP_TYPE_PNG);
+        image.Rescale(50, 50);
+
+        wxPoint pos(200, 10);
+
+        if(!bm.empty() )
+        {
+            pos = bm.top()->GetPosition();
+            pos.x += 60;
+            if(pos.x > 400)
+            {
+                pos.x = 200;
+                pos.y += 60;  
+            } 
+        }
+        wxStaticBitmap sbm(this, wxID_ANY, wxBitmap(image, wxBITMAP_TYPE_PNG), pos);
+        wxBitmapButton* bmb = new wxBitmapButton(this, wxID_ANY, sbm.GetBitmap(), pos);
+        bm.emplace(bmb);
+        // bm.emplace( new wxStaticBitmap(this, wxID_ANY, wxBitmap(image, wxBITMAP_TYPE_PNG), pos) );
     }
 
     evt.Skip();
+};
+
+void LoadAssetsPanel::OnImageClick(wxCommandEvent& evt) 
+{
+    int x = wxGetMousePosition().x;
+    printf("mouse x: %i\n", x);   
 };
--- LoadAssetsPanel.h
+++ LoadAssetsPanel.h
@@ -1,14 +1,20 @@
 #ifndef LOADASSETSPANEL_H
 #define LOADASSETSPANEL_H
 
-#include <string>
+#include <stack>
+
 #include <wx/wx.h>
+#include <wx/vscroll.h>
 
 class LoadAssetsPanel : public wxPanel
 {
     private:
         wxButton* loadAssetsBtn = nullptr;
+        wxHVScrolledWindow* m_assetsWindow = nullptr;
+        // std::stack<wxStaticBitmap*> bm;
+        std::stack<wxBitmapButton*> bm;
         void OnLoadBtnClick(wxCommandEvent& evt);
+        void OnImageClick(wxCommandEvent& evt);
 
         wxDECLARE_EVENT_TABLE();
     
