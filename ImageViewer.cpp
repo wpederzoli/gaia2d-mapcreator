82029a0ecf670df131fb5ad5a19b0170cf905e69
William Pederzoli <wpederzoli@gmail.com>
2022-03-14 14:30:58 +0100

    Unused funciton (refatored) deleted

--- ImageViewer.cpp
+++ ImageViewer.cpp
@@ -122,67 +122,6 @@ void ImageViewer::OnMouseMove(wxMouseEvent& evt)
     evt.Skip();    
 };
 
-void ImageViewer::DrawMouse(wxDC& dc, int x, int y)
-{
-    wxBrush b = dc.GetBrush();
-    b.SetColour(wxColor(0, 0, 0, 0) );
-    dc.SetBrush(b);
-    
-    wxPen p = dc.GetPen();
-    p.SetStyle(wxPENSTYLE_LONG_DASH);
-    p.SetColour(wxColor(255, 0, 0, 50) );
-    
-    if(m_mouseDownPos.x > x && m_mouseDownPos.x < x + m_tileSize && m_mouseDownPos.y > y && m_mouseDownPos.y < y + m_tileSize)
-    {
-        p.SetStyle(wxPENSTYLE_SOLID);
-        p.SetColour(wxColor(0, 0, 255) );
-    }
-
-    if(m_mousePos.x > x && m_mousePos.x < x + m_tileSize && m_mousePos.y > y && m_mousePos.y < y + m_tileSize)
-    {
-        p.SetStyle(wxPENSTYLE_SOLID);
-        p.SetColour(wxColor(0, 0, 255) );
-    }
-
-    dc.SetBackground(b);
-    dc.SetPen(p);
-    dc.DrawRectangle(wxPoint(x, y), wxSize(m_tileSize, m_tileSize) );
-
-    if(m_mouseDown)
-    {
-        p.SetColour(wxColor(80, 150, 80) );
-        b.SetColour(wxColor(100, 130, 20, 1) );
-        dc.SetBrush(b);
-        dc.SetBackground(b);
-        dc.SetPen(p);
-        dc.DrawRectangle(m_mouseDownPos, wxSize(m_mousePos.x - m_mouseDownPos.x, m_mousePos.y - m_mouseDownPos.y) );
-    }
-    else
-    {
-        wxRect rect(m_mouseDownPos, m_mouseUpPos);
-        wxBitmap bitx = dc.GetAsBitmap(&rect);
-        dc.DrawBitmap(bitx, m_mousePos);
-    }
-
-    //update selection to match tile size
-    if(m_mouseDownPos.x > x && m_mouseDownPos.x < x + m_tileSize && m_mouseDownPos.y > y && m_mouseDownPos.y < y + m_tileSize)
-    {
-        m_mouseDownPos.x = x;
-        m_mouseDownPos.y = y;
-    }
-
-    if(m_mouseUpPos.x > x && m_mouseUpPos.x < x + m_tileSize && m_mouseUpPos.y > y && m_mouseUpPos.y < y + m_tileSize)
-    {
-        m_mouseUpPos.x = x + m_tileSize;
-        m_mouseUpPos.y = y + m_tileSize;
-    }
-
-    b.SetColour(wxColor(100, 200, 100, 1) );
-    dc.SetBrush(b);
-    dc.DrawRectangle(m_mouseDownPos, wxSize(m_mouseUpPos.x - m_mouseDownPos.x, m_mouseUpPos.y - m_mouseDownPos.y) );
-    
-};
-
 void ImageViewer::OnMouseClick(wxMouseEvent& evt) 
 {
     m_selected = !m_selected;
