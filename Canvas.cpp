134047e1abd79edf34c7243bf1512f778e7cb545
William Pederzoli <wpederzoli@gmail.com>
2022-03-17 12:43:50 +0100

    Canvas draw refactor. Mouse sprite draw scroll bug fix

--- Canvas.cpp
+++ Canvas.cpp
@@ -3,6 +3,9 @@
 wxBEGIN_EVENT_TABLE(Canvas, wxHVScrolledWindow)
     EVT_PAINT(Canvas::OnPaint)
     EVT_MOTION(Canvas::OnMouseMove)
+    EVT_LEFT_DOWN(Canvas::OnMouseDown)
+    EVT_RIGHT_DOWN(Canvas::OnMouseRightDown)
+    EVT_LEFT_UP(Canvas::OnMouseUp)
 wxEND_EVENT_TABLE()
 
 Canvas::Canvas(wxWindow* parent, int cols, int rows, int ts) : wxHVScrolledWindow(parent, wxID_ANY) 
@@ -16,37 +19,6 @@ Canvas::Canvas(wxWindow* parent, int cols, int rows, int ts) : wxHVScrolledWindo
 
 Canvas::~Canvas(){};
 
-void Canvas::OnDraw(wxDC& dc) 
-{
-    dc.Clear();
-
-    wxBrush brush = dc.GetBrush();
-    wxPen pen = dc.GetPen();
-
-    wxPosition s = GetVisibleBegin();
-    wxPosition e = GetVisibleEnd();
-
-    pen.SetStyle(wxPENSTYLE_LONG_DASH);
-    pen.SetColour(wxColor(200, 200, 200));
-    dc.SetPen(pen);
-
-    for(int y = s.GetRow(); y < e.GetRow(); y++)
-        for(int x = s.GetCol(); x < e.GetCol(); x++)
-        {
-            dc.SetBrush(brush);
-            dc.DrawRectangle(x*m_tileSize, y*m_tileSize, m_tileSize, m_tileSize);
-        }
-
-    if(m_activeBitmapLoaded)
-    {
-        wxImage i = m_activeBitmap.ConvertToImage();
-        i.Rescale( (i.GetWidth()/m_initialTileSize)*m_tileSize, (i.GetHeight()/m_initialTileSize)*m_tileSize);
-        wxBitmap bm(i);
-        dc.DrawBitmap(bm, m_mousePosition);
-        Refresh();
-    }
-};
-
 void Canvas::OnPaint(wxPaintEvent& evt) 
 {
     wxBufferedPaintDC dc(this);
@@ -54,9 +26,49 @@ void Canvas::OnPaint(wxPaintEvent& evt)
     OnDraw(dc);
 };
 
+void Canvas::OnDraw(wxDC& dc) 
+{
+    dc.Clear();
+
+    wxBrush brush = dc.GetBrush();
+    brush.SetColour(wxColor(0, 0, 0, 0) );
+    dc.SetBrush(brush);
+    
+    wxPen pen = dc.GetPen();
+    pen.SetStyle(wxPENSTYLE_LONG_DASH);
+    pen.SetColour(wxColor(100, 100, 100) );
+    dc.SetPen(pen);
+
+    DrawBackground(dc);
+    DrawGrid(dc);
+    DrawActiveSprite(dc);
+};
+
 void Canvas::OnMouseMove(wxMouseEvent& evt) 
 {
+    wxPosition p = GetVisibleBegin();
     m_mousePosition = evt.GetPosition();
+    m_mousePosition.x += (p.GetCol()*m_tileSize);
+    m_mousePosition.y += (p.GetRow()*m_tileSize);
+    evt.Skip();
+};
+
+void Canvas::OnMouseDown(wxMouseEvent& evt)
+{
+    wxImage i(m_activeBitmap.ConvertToImage() );
+    i.Rescale( (i.GetWidth()/m_initialTileSize)*m_tileSize, (i.GetHeight()/m_initialTileSize)*m_tileSize );
+    m_tmpBitmap = wxBitmap(i);
+    evt.Skip();
+};
+
+void Canvas::OnMouseUp(wxMouseEvent& evt) 
+{
+    evt.Skip();
+};
+
+void Canvas::OnMouseRightDown(wxMouseEvent& evt)
+{
+    evt.Skip();  
 };
 
 wxCoord Canvas::OnGetColumnWidth(size_t col) const 
@@ -85,6 +97,52 @@ void Canvas::setPixelSize(int n)
 
 void Canvas::SetActiveBitmap(wxBitmap& bm) 
 {
-    m_activeBitmapLoaded = true;
     m_activeBitmap = bm; 
+};
+
+void Canvas::DrawBackground(wxDC& dc)
+{
+    if(m_mapBitmap.IsOk() )
+    {
+        // wxImage i = m_mapBitmap.ConvertToImage();
+        // i.Rescale(GetRowCount()*m_tileSize, GetColumnCount()*m_tileSize);
+        // wxBitmap bm(i);
+        dc.DrawBitmap(m_mapBitmap, wxDefaultPosition);
+    }
+    
+    if(m_tmpBitmap.IsOk() )
+    {   
+        wxPen p = dc.GetPen();
+        p.SetColour(wxColor(255, 255, 255, 0) );
+        dc.SetPen(p);
+
+        //Scale bitmap
+        dc.DrawBitmap(m_tmpBitmap, m_mousePosition);   
+        m_mapBitmap = dc.GetAsBitmap();
+        m_tmpBitmap = wxBitmap();
+        
+        Refresh();
+    }
+};
+
+void Canvas::DrawGrid(wxDC& dc) 
+{
+    wxPosition s = GetVisibleBegin();
+    wxPosition e = GetVisibleEnd();
+
+    for(int y = s.GetRow(); y < e.GetRow(); y++)
+        for(int x = s.GetCol(); x < e.GetCol(); x++)
+            dc.DrawRectangle(x*m_tileSize, y*m_tileSize, m_tileSize, m_tileSize);
+};
+
+void Canvas::DrawActiveSprite(wxDC& dc) 
+{
+    if(m_activeBitmap.IsOk() )
+    {
+        wxImage i = m_activeBitmap.ConvertToImage();
+        i.Rescale( (i.GetWidth()/m_initialTileSize)*m_tileSize, (i.GetHeight()/m_initialTileSize)*m_tileSize );
+        wxBitmap bm(i);
+        dc.DrawBitmap(bm, m_mousePosition);
+        Refresh();
+    }
 };
--- Canvas.h
+++ Canvas.h
@@ -1,31 +1,43 @@
 #ifndef CANVAS_H
 #define CANVAS_H
 
+#include <list>
+
 #include <wx/wx.h>
 #include <wx/vscroll.h>
 #include <wx/dcclient.h>
 #include <wx/dcmemory.h>
 #include <wx/dcbuffer.h>
 
+#include "MapImage.h"
+
 class Canvas : public  wxHVScrolledWindow
 {
     private:
         virtual wxCoord OnGetRowHeight(size_t row) const;
         virtual wxCoord OnGetColumnWidth(size_t col) const;
-
-        void OnDraw(wxDC& dc);
-        void OnPaint(wxPaintEvent& evt);
-
+        
         int m_cols = 0;
         int m_rows = 0;
         int m_tileSize = 0;
         int m_initialTileSize = 0;
-        bool m_activeBitmapLoaded = false;
 
+        void OnDraw(wxDC& dc);
+        void OnPaint(wxPaintEvent& evt);
+        void OnMouseMove(wxMouseEvent& evt);
+        void OnMouseDown(wxMouseEvent& evt);
+        void OnMouseRightDown(wxMouseEvent& evt);
+        void OnMouseUp(wxMouseEvent& evt);
+
+        wxBitmap m_mapBitmap;
         wxBitmap m_activeBitmap;
+        wxBitmap m_tmpBitmap;
+
         wxPoint m_mousePosition;
 
-        void OnMouseMove(wxMouseEvent& evt);
+        void DrawBackground(wxDC& dc);
+        void DrawGrid(wxDC& dc);
+        void DrawActiveSprite(wxDC& dc);
 
         wxDECLARE_EVENT_TABLE();
     
